c47266b7470fbc2d5793ce1759d167eb92b37ad9
lhs0698 <yohs0698@naver.com>
2023-09-03 01:38:06 +0900

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+
+export default function App({ Component, pageProps }) {
+
+  const EmptyLayout = ({children}) => <>{children}</>;
+  const SubLayout = Component.Layout || EmptyLayout
+
+  return (
+    <Layout>
+      <SubLayout>
+        <Component {...pageProps} />
+      </SubLayout>
+    </Layout>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Home() {
+
+  const code = "KOR";
+  const router = useRouter();
+  
+  const onClickButton = () => {
+    router.push({
+      pathname : "country/[code]",
+      query : { code : code}
+    })
+  }
+
+  return (
+    <div>
+      Home Page
+      <div>
+        <button onClick={onClickButton}>Search Page로 이동</button>
+      </div>
+      <div>
+        <Link href={"/search"}>Search Page 이동</Link>
+      </div>
+      <div>
+        {/* <Link href={`/country/${code}`}>{code}페이지로 이동 </Link> */}
+        <Link href={{
+          pathname : "/country/[code]",
+          query : {code : code}
+        }}>{code} 페이지로 이동</Link>
+      </div>
+    </div>
+  );
+}
